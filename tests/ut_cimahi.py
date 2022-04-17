9828062e1ca766b35a805697dfd4db0056dfd622
dudung <dudung@gmail.com>
2022-04-17 22:51:58 +0700

    test cimahi update, pause

--- tests/ut_cimahi.py
+++ tests/ut_cimahi.py
@@ -13,3 +13,15 @@ print()
 print("Cimahi Utara population in 2021")
 print(cimu.population['2021'])
 print()
+
+print("Cimahi Selatan PDRB ADHK sector A in 2016")
+print(cims.pdrb_adhk['A']['2016'])
+print()
+
+print("Cimahi Selatan PDRB ADHK sector C in 2016")
+print(cims.pdrb_adhk['C']['2016'])
+print()
+
+print("Cimahi Selatan PDRB ADHK sector C in 2017")
+print(cims.pdrb_adhk['C']['2017'])
+print()
