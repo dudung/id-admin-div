6619e17195a990b80be8ba8109b254a939c1b14d
dudung <dudung@gmail.com>
2022-04-17 22:39:13 +0700

    test cimahi

---
+++ tests/ut_cimahi.py
@@ -0,0 +1,15 @@
+import id_data.jawabarat.cimahi.cimahiselatan as cims
+import id_data.jawabarat.cimahi.cimahitengah as cimt
+import id_data.jawabarat.cimahi.cimahiutara as cimu
+
+print("Cimahi Selatan population in 2021")
+print(cims.population['2021'])
+print()
+
+print("Cimahi Tengah population in 2021")
+print(cimt.population['2021'])
+print()
+
+print("Cimahi Utara population in 2021")
+print(cimu.population['2021'])
+print()
