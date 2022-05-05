644490139b54ad18df6cbed6409d8590cf64ffba
dudung <dudung@gmail.com>
2022-05-05 22:03:21 +0700

    rename

--- tests/ut_idadmindiv_lev1.py
+++ tests/ut_idadmindiv_lev1.py
@@ -1,9 +1,9 @@
 # 
-# ut_idadmindiv_ac.py
-# Test idadmindiv package for ac province information
+# ut_idadmindiv_lev1.py
+# Test idadmindiv package for province information
 # Sparisoma Viridi | https://github.com/dudung
 # 
-# Execute: python -m tests.idadmindiv_ac
+# Execute: python -m tests.idadmindiv_lv1
 # 
 # 20220505 create this test file and modify __init__.py file
 # 
