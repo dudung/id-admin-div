fe558aa7a5b72220282066a05d8f4d2a36e7ed2c
dudung <dudung@gmail.com>
2022-05-05 21:41:22 +0700

    fin ja ri

---
+++ idadmindiv/ja/info.py
@@ -0,0 +1,6 @@
+level = 1
+name = 'Jambi'
+capital = 'Jambi'
+area = 50160.05
+code = 15
+iso = 'id-ja'
---
+++ idadmindiv/ri/info.py
@@ -0,0 +1,6 @@
+level = 1
+name = 'Riau'
+capital = 'Pekanbaru'
+area = 87023.66
+code = 14
+iso = 'id-ri'
